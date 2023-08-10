7e24cacd23ca137b55ad3d427b0a3d45b4efd16f
wy85 <wy85@drexel.edu>
2023-08-10 15:25:29 -0400

    rebase

--- app/constants.js
+++ app/constants.js
@@ -1,17 +1,22 @@
 const PLAYERS_PER_GAME = 2;
 const ROLES = {
-    HUMAN : "Human",
-    AI: "Ai"
-}
-const ANONY_COOKIE_DURATION_MS = 5*60*1000; // 5 mins
-
+  HUMAN: "Human",
+  AI: "Ai",
+};
+const ANONY_COOKIE_DURATION_MS = 5 * 60 * 1000; // 5 mins
 
 const HUMAN_ACTIONS = {
-    enterRoom : "enterRoom",
-    getCurrentRoom : "getCurrentRoom"
-}
+  enterRoom: "enterRoom",
+  getCurrentRoom: "getCurrentRoom",
+};
 const AI_ACTIONS = {
-    getCurrentRoom : "getCurrentRoom"
-}
+  getCurrentRoom: "getCurrentRoom",
+};
 
-module.exports = { PLAYERS_PER_GAME, ROLES, ANONY_COOKIE_DURATION_MS, HUMAN_ACTIONS, AI_ACTIONS };
+module.exports = {
+  PLAYERS_PER_GAME,
+  ROLES,
+  ANONY_COOKIE_DURATION_MS,
+  HUMAN_ACTIONS,
+  AI_ACTIONS,
+};
--- app/public/script.js
+++ app/public/script.js
@@ -1,12 +1,51 @@
+const e = require("express");
+
 const webSocket = new WebSocket(`ws://localhost:${WEBSOCKET_PORT}/`);
 webSocket.onmessage = (event) => {
   console.log(event.data);
 };
 webSocket.addEventListener("open", () => {
   console.log("We are connected");
+  sendMessage("Hi from client");
 });
-const sendMessage = () => {
+
+const sendMessage = (message) => {
   // Sends message to websocket server
-  webSocket.send("Hi from client");
+  // "Hi from client"
+  webSocket.send(message);
+};
+
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    loginUser(username, password);
+  });
+
+function loginUser(username, password) {
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else if (response.status === 400) {
+        document.getElementById("error-message").innerText =
+          "Incorrect username or password, please try again";
+        throw new Error("Incorrect username or password");
+      }
+    })
+    .catch((error) => {
+      console("There was an error logging in" + error);
+    });
 }
-document.getElementById('send').addEventListener('click', sendMessage);
