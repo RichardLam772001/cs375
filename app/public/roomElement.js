0866c96980fd5de375acb6ba9f1ff53f5fe56102
Colin Page <colinjpage20@gmail.com>
2023-08-14 02:54:03 -0400

    Threat images inside rooms

--- app/public/roomElement.js
+++ app/public/roomElement.js
@@ -7,6 +7,11 @@ const RoomElement = (parentElem) => {
     const backElem = createElemUnderParent("div", mainDiv);
     backElem.className = "visible";
 
+    const threatImg = createElemUnderParent("img", backElem);
+    setThreat("none");
+    const threats = ["fire", "breach", "invader", "none"];
+    setInterval(() => setThreat(threats[Math.floor(Math.random()*threats.length)]), 1000);
+
 
     function setVisible(visible){
         backElem.className = visible ? "visible" : "hidden";
@@ -16,6 +21,23 @@ const RoomElement = (parentElem) => {
         const newElem = document.createElement(elemType);
         parentElem.appendChild(newElem);
         return newElem;
+    }
+
+    function setThreat(threatString){
+        threatImg.style.visibility = "visible";
+        switch(threatString){
+            case "fire":
+                threatImg.src = "../images/Fire.png";
+                break;
+            case "breach":
+                threatImg.src = "../images/Breach.png";
+                break;
+            case "invader":
+                threatImg.src = "../images/Invader.png";
+                break;
+            default:
+                threatImg.style.visibility = "hidden";
+        }
     }
 
     return {
