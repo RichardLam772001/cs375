965e1942dec54298ced57300c1bbfd0d7b9de3be
Ubuntu <ubuntu@ip-172-31-95-190.ec2.internal>
2023-08-20 16:38:38 +0000

    Add configuration to setup websockets and AWS postgres database secret for EC2 instance

--- app/public/constants.js
+++ app/public/constants.js
@@ -1,4 +1,6 @@
 const WEBSOCKET_PORT = 443;
+const ELASTIC_IP_ADDRESS = "54.172.133.168";
+const IS_PROD = true;
 
 const ROLES = {
     HUMAN : "Human",
--- app/server.js
+++ app/server.js
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 app.use(express.json());
 
 app.listen(PORT, () => {
-	console.log(`App running on http://:${PORT}`);
+	console.log(`App running on http://${HOSTNAME}:${PORT}`);
 });
 
 setRequestHandlers(app);
