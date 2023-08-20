0bac511c68f294b185bd6a98797f9d93aca0545e
unknown <richardlam772001@gmail.com>
2023-08-20 13:08:32 -0400

    Update README

--- app/public/constants.js
+++ app/public/constants.js
@@ -1,6 +1,6 @@
 const WEBSOCKET_PORT = 443;
 const ELASTIC_IP_ADDRESS = "54.172.133.168";
-const IS_PROD = true;
+const IS_PROD = false;
 
 const ROLES = {
     HUMAN : "Human",
--- app/server.js
+++ app/server.js
@@ -1,6 +1,6 @@
 // Require statements
 const { setRequestHandlers } = require("./request_handlers/request_handlers.js");
-const { HOSTNAME, PORT } = require("../env.json");
+const { PROD_HOSTNAME, LOCAL_HOSTNAME, PROD_PORT, LOCAL_PORT, IS_PROD } = require("../env.json");
 
 const express = require("express");
 const cookieParser = require("cookie-parser");
@@ -12,8 +12,11 @@ app.use(cookieParser());
 app.use(express.static("public"));
 app.use(express.json());
 
-app.listen(PORT, () => {
-	console.log(`App running on http://${HOSTNAME}:${PORT}`);
+const hostname = IS_PROD ? PROD_HOSTNAME : LOCAL_HOSTNAME;
+const port = IS_PROD ? PROD_PORT : LOCAL_PORT;
+
+app.listen(port, () => {
+	console.log(`App running on http://${hostname}:${port}`);
 });
 
 setRequestHandlers(app);
