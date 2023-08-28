79e7dbb17f0cbeb20ab55e47a7ef6018ed485385
unknown <richardlam772001@gmail.com>
2023-08-28 16:31:05 -0400

    Change game time

--- app/game/game.js
+++ app/game/game.js
@@ -35,7 +35,7 @@ const GAME = (humanUsername, aiUsername, gameId) => {
 
     const GAME_ID = gameId;
 
-    let gameTime = 4*60;
+    let gameTime = 2*60;
     let HUMAN_USERNAME = humanUsername;
     let AI_USERNAME = aiUsername;
     let room = "0-0";
